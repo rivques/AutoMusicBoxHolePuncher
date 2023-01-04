4e02ab9d77d3efcd91df9a12e58b8ac3f0af8be4
rivques <38469076+rivques@users.noreply.github.com>
2023-01-04 14:21:02 -0500

    begin async work

--- code.py
+++ code.py
@@ -1,15 +1,37 @@
 import time # pyright: reportShadowedImports=false
-from utils import *
+from lib.utils import *
 import asyncio
 from adafruit_motor.stepper import StepperMotor
+from adafruit_motor.servo import Servo
 import sys
+from pwmio import PWMOut
+import board
+import adafruit_logging
 
 class HolePuncher:
+    logger = adafruit_logging.getLogger()
+    motor_logger = adafruit_logging.Logger("motor", 10) # level 10 is debug level
     
     _hole_puncher_state = HolePuncherState.OFF
 
-    # hardware
-    x_stepper = StepperMotor()
+    # # hardware
+    # x_stepper_pins = [
+    #     PWMOut(board.D2),
+    #     PWMOut(board.D3),
+    #     PWMOut(board.D4),
+    #     PWMOut(board.D5)
+    # ]
+    # x_stepper = StepperController(StepperMotor(*x_stepper_pins))
+    # y_stepper_pins = [
+    #     PWMOut(board.D6),
+    #     PWMOut(board.D7),
+    #     PWMOut(board.D8),
+    #     PWMOut(board.D9)
+    # ]
+    # y_stepper = StepperController(StepperMotor(*y_stepper_pins))
+    # z_servo_a = Servo(PWMOut(board.D10, duty_cycle=2 ** 15, frequency=50), min_pulse=500, max_pulse=2500)
+    # z_servo_b = Servo(PWMOut(board.D11, duty_cycle=2 ** 15, frequency=50), min_pulse=500, max_pulse=2500)
+    # drill_motor = PWMOut(board.D12)
 
     @property
     def hole_puncher_state(self):
@@ -19,7 +41,7 @@ class HolePuncher:
     def hole_puncher_state(self, newValue):
         # here's the on state change code
         if newValue == HolePuncherState.OFF:
-            pass
+            asyncio.get_event_loop().stop()
         elif newValue == HolePuncherState.STARTUP:
             pass
         elif newValue == HolePuncherState.IDLE:
@@ -32,6 +54,7 @@ class HolePuncher:
 
     def __init__(self) -> None:
         self.hole_puncher_state = HolePuncherState.STARTUP
+        self.logger.setLevel(10)
 
     async def run_ui(self):
         # ui loop, will call itself again if it's still turned on
@@ -44,9 +67,23 @@ class HolePuncher:
         pass
     
     def punch_holes(operations):
+        self.hole_puncher_state = HolePuncherState.PUNCHING
         # given a list of Operations, execute them
-        pass
+        for operation in operations:
+            # again, rust match would be so nice here
+            if operation.operationType == OperationType.PROGRAMSTART:
+                pass
+async def async_test():
+    while True:
+        print("Hello async!")
+        await asyncio.sleep(1)
 
-if __name__ == "__main__":
+async def input_test():
+    theInput = await ainput("aInput!")
+    print(theInput)
+asyncio.create_task(async_test())
+asyncio.run(input_test())
+if False and __name__ == "__main__":
     holePuncher = HolePuncher()
     asyncio.run(holePuncher.run_ui())
+    asyncio.get_event_loop().run_forever()
---
+++ lib/utils.py
@@ -0,0 +1,73 @@
+import time
+import adafruit_motor.stepper as stepper
+import asyncio
+import adafruit_logging
+import sys
+
+def enum(*enum_keys): # https://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
+    enums = {enum_key: enum_key for enum_key in enum_keys}
+    enums["keys"] = enums.keys()
+    return type('Enum', (), enums)
+
+async def ainput(string: str) -> str:
+    sys.stdout.write(string+' ')
+    theInput = ""
+    while "\n" not in theInput:
+        await asyncio.sleep(0)
+        #test for thing on line
+        theNewInput = sys.stdin.read(1)
+        sys.stdout.write(theNewInput)
+        theInput += theNewInput
+    return theInput
+            
+
+HolePuncherState = enum("OFF", "STARTUP", "IDLE", "PUNCHING")
+
+OperationType = enum("PROGRAMSTART", "PROGRAMEND", "PUNCHNOTE", "ADVANCEPAPER")
+
+class Operation: # i really wish I could use Rust enums for this, they're perfect for the usecase
+    def __init__(self, operationType, operationValue):
+        self.operationType = operationType
+        self.operationValue = operationValue
+        self.logger = adafruit_logging.getLogger("motor")
+
+class StepperController:
+    def __init__(self, in_stepper, mm_per_degree = .1, degrees_per_tick = 1.8, lower_step_limit = 0, upper_step_limit = 1000): # TODO: Improve this with actual numbers
+        self.stepper: stepper.StepperMotor = in_stepper
+        self.ticks: float = 0
+        self.degrees_per_tick = degrees_per_tick
+        self.mm_per_degree = mm_per_degree
+        self.lower_step_limit = lower_step_limit
+        self.upper_step_limit = upper_step_limit
+        self.is_running = False
+    
+    async def go_to_step(self, steps): # takes an absolute position in steps and tries to go there
+        self.is_running = True
+        SECONDS_PER_STEP = .05 # this is overly slow, for testing
+        if steps == self.ticks: return
+        direction = stepper.FORWARD if steps > self.ticks else stepper.BACKWARD
+        # first of all, get to an integer number of steps
+        whole_steps, decimal_steps = divmod(self.ticks, 1)
+        microsteps_needed = ((1.0-decimal_steps)/16.0) if direction == stepper.FORWARD else (decimal_steps/16.0)
+        self.logger.debug(f"Need {microsteps_needed} microsteps then {whole_steps} whole steps {'forward' if direction == stepper.FORWARD else 'backward'}")
+        for i in range(microsteps_needed):
+            self.stepper.onestep(direction=direction, style=stepper.MICROSTEP)
+            self.ticks += (1 if direction == stepper.forward else -1) * 1/16
+            await asyncio.sleep(SECONDS_PER_STEP)
+        # now normal steps
+        assert self.ticks - int(self.ticks) == 0, "position should be a whole number"
+        for i in range(abs(whole_steps - int(self.ticks))):
+            self.stepper.onestep(direction=direction, style=stepper.DOUBLE)
+            self.ticks += 1 if direction == stepper.forward else -1
+            await asyncio.sleep(SECONDS_PER_STEP)
+        self.is_running = False
+    
+    async def go_to_position(self, position):
+        await self.go_to_step(self.mm_per_degree * self.degrees_per_tick * position)
+    
+    def redefine_position(self, new_position):
+        if self.is_running:
+            return False # could not change position while motor is running
+        self.position = new_position
+        return True
+        
